2bf3cbce173e2a31c520189dd5639bee59a287bd
sergiyko12 <sergplis@mail.ru>
2018-12-25 17:18:20 +0300

    fix import

--- shell.go
+++ shell.go
@@ -8,8 +8,8 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gorillalabs/go-powershell/backend"
-	"github.com/gorillalabs/go-powershell/utils"
+	"github.com/sergiyko12/go-powershell/backend"
+	"github.com/sergiyko12/go-powershell/utils"
 	"github.com/juju/errors"
 )
 
